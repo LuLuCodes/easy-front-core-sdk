0922ef6019c505304b50cce0e629c59d8d990fa5
qian.qing@aliyun.com <qian.qing@aliyun.com>
2021-02-05 16:00:08 +0800

    fix(fix bug): fix bug
    affects: @easy-front-core-sdk/opencpwx

--- packages/opencpwx/src/api/AccessTokenDecorator.ts
+++ packages/opencpwx/src/api/AccessTokenDecorator.ts
@@ -2,7 +2,10 @@ export const AccessTokenRefresh = (): MethodDecorator => (target: any, propertyK
   const originalMethod = descriptor.value
   descriptor.value = async function (...args: any[]) {
     let result = await originalMethod.apply(this, args)
-    if (result.errcode === 40014) {
+    if (result.errcode) {
+      console.error(result)
+    }
+    if (result.errcode === 40014 || result.errcode === 42001 || result.errcode === 42009 || result.errcode === 40082) {
       // access token 失效后重新获取
       await args[0].refreshAccessToken()
       result = await originalMethod.apply(this, args)
