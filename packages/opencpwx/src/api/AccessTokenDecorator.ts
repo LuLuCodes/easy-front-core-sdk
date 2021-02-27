e1b1c53018aa0c6b124d72b2f172c4f2f4772420
qian.qing@aliyun.com <qian.qing@aliyun.com>
2021-02-27 13:15:03 +0800

    fix(错误消息): 错误消息
    affects: @easy-front-core-sdk/opencpwx

--- packages/opencpwx/src/api/AccessTokenDecorator.ts
+++ packages/opencpwx/src/api/AccessTokenDecorator.ts
@@ -1,18 +1,23 @@
+import { ErrorMsg } from './ErrorMsg'
 export const AccessTokenRefresh = (): MethodDecorator => (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
   const originalMethod = descriptor.value
   descriptor.value = async function (...args: any[]) {
     let result = await originalMethod.apply(this, args)
     if (!result) {
-      throw new Error('接口异常')
+      throw new Error('接口异常(网络异常)')
+    }
+    if (result.errcode === 40014 || result.errcode === 42001 || result.errcode === 42009 || result.errcode === 40082) {
+      // access token 失效后重新获取
+      await args[0].refreshAccessToken()
+      result = await originalMethod.apply(this, args)
     }
     if (result.errcode) {
       console.error(`${propertyKey} error: ${JSON.stringify(result)}`)
-      if (result.errcode === 40014 || result.errcode === 42001 || result.errcode === 42009 || result.errcode === 40082) {
-        // access token 失效后重新获取
-        await args[0].refreshAccessToken()
-        result = await originalMethod.apply(this, args)
-      } else {
+      const errmsg = ErrorMsg[result.errcode]
+      if (errmsg) {
         throw new Error(result.errmsg)
+      } else {
+        throw new Error('接口异常(未知错误)')
       }
     }
     delete result.errcode
